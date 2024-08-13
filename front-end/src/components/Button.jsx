32bbef8100083491a0f706b763310fa2bfbd1f6b
Herinandrianina Eloi Charly <86348283+Benandry@users.noreply.github.com>
2024-08-13 16:06:13 +0200

    dev: create component for button and input

--- front-end/src/components/Button.jsx
+++ front-end/src/components/Button.jsx
@@ -1,11 +1,13 @@
-import { Link } from "react-router-dom";
-
-function Button({ to, label }) {
+function Button({ type, label, color, onClick }) {
   return (
-    <Link to={to} className="p-3 m-5 rounded-sm text-white bg-blue-600">
-      {label}
-    </Link>
+    <div>
+      <button
+        type={type}
+        className={`text-white bg-${color}-700 hover:bg-${color}-800 focus:ring-4 focus:outline-none focus:ring-${color}-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-${color}-600 dark:hover:bg-${color}-700 dark:focus:ring-${color}-800`}
+      >
+        {label}
+      </button>
+    </div>
   );
 }
-
 export default Button;
