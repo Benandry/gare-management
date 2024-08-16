a2b3c3542369bbbb095f60ee6ac618ad52199e36
Herinandrianina Eloi Charly <86348283+Benandry@users.noreply.github.com>
2024-08-16 16:10:14 +0200

    dev: add in menu sidebar

--- front-end/src/components/sidebar/index.jsx
+++ front-end/src/components/sidebar/index.jsx
@@ -9,34 +9,52 @@ function Sidebar({ children }) {
           <div className=" h-full basis-1/2 gap-7 flex flex-col ">
             <AccordionItem title="Voyageurs">
               <div className="flex flex-col gap-2 ">
-                <Link to="/traveler" className="font-normal text-sm">
-                  Liste du Voyageur
+                <Link to="/traveler" className="font-normal text-sm  my-2">
+                  → Liste du Voyageur
                 </Link>
-                <Link to="/traveler/create" className="font-normal text-sm">
-                  Créer un voyageur
+                <Link
+                  to="/traveler/create"
+                  className="font-normal text-sm  my-2"
+                >
+                  → Créer un voyageur
                 </Link>
               </div>
             </AccordionItem>
             <AccordionItem title="Réservation">
               <div className="flex flex-col gap-2 ">
-                <Link to="#" className="font-normal text-sm">
-                  Liste du réservation
+                <Link to="#" className="font-normal text-sm  my-2">
+                  → Liste du réservation
                 </Link>
-                <Link to="#" className="font-normal text-sm">
-                  Créer un réservation
+                <Link to="#" className="font-normal text-sm  my-2">
+                  → Créer un réservation
                 </Link>
               </div>
             </AccordionItem>
             <AccordionItem title="Gestion de Trajets">
               <div className="flex flex-col gap-2 ">
-                <Link to="#" className="font-normal text-sm">
-                  Liste du trajets
+                <Link to="#" className="font-normal text-sm  my-2">
+                  → Liste du trajets
                 </Link>
-                <Link to="#" className="font-normal text-sm">
-                  Créer un trajets
+                <Link to="#" className="font-normal text-sm  my-2">
+                  → Créer un trajets
                 </Link>
               </div>
             </AccordionItem>
+            <div className="mb-5">
+              <AccordionItem title="Parametres">
+                <div className="flex flex-col gap-2 ">
+                  <Link to="/settings/car" className="font-normal text-sm my-2">
+                    → Gestion de Voitures
+                  </Link>
+                  <Link
+                    to="/settings/driver"
+                    className="font-normal text-sm  my-2"
+                  >
+                    → Chauffeurs
+                  </Link>
+                </div>
+              </AccordionItem>
+            </div>
           </div>
         </div>
         <div className="w-4/5 py-4 px-6 h-full overflow-auto">{children}</div>
